3d4fc849408ac4bc4393f9dbdab9fa2d870f1377
Faheem <faheemtech1@gmail.com>
2019-01-29 16:43:58 +0530

    django setup added with Django-Rest framework

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/faheem/development/full-stack-projects/react-django-template/backend/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
