30c07bd3412e5f2369bcbbe194a11c31d23c36df
Faheem <faheemtech1@gmail.com>
2019-01-29 18:08:11 +0530

    added a super user and login for admin panel

--- backend/src/login/models.py
+++ backend/src/login/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Login(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
